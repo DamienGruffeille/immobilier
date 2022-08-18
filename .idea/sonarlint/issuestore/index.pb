71ca439538a562392808bacb03e6bd14f8aa9563
Damien Gruffeille <damien.gruffeille@gmail.com>
2022-08-18 11:29:07 +0200

    Ajout fenetre JavaFX et remplissage cbx avec données de la BDD

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
+L
+.idea/codeStyles/Project.xml,6\e\6ecd6000a7b6f4a2884412ff19f74193ed089648
+U
+%.mvn/wrapper/maven-wrapper.properties,3\2\32e074f71d16c1e88ac02aa83592ca6ad1fd98a8
+8
+mvnw.cmd,1\f\1f1763f358c257ea3515417352b82b0165647ad1
+4
+mvnw,5\d\5d837c4ed85ecaaf932c506e80ff5d7b9f3d590d
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/main/java/fr/afpa/immobilier/controllers/Controller.java
+++ src/main/java/fr/afpa/immobilier/controllers/Controller.java
@@ -1,14 +1,88 @@
 package fr.afpa.immobilier.controllers;
 
+import fr.afpa.immobilier.data.AppDataSource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnEffacer;
+
+    @FXML
+    private Button btnValider;
+
+    @FXML
+    private ComboBox<String> cbxImmeuble;
+
+    @FXML
+    private Label lblAdresse;
+
+    @FXML
+    private Label lblNbPieces;
+
+    @FXML
+    private Label lblNumAppart;
+
+    @FXML
+    private Label lblNumImmeuble;
+
+    @FXML
+    private Label lblPrix;
+
+    @FXML
+    private Label lblSurface;
+
+    @FXML
+    private Label lblVille;
+
+    @FXML
+    void effacer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechercherAppart(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        remplirCbx();
+    }
+
+    private void remplirCbx(){
+
+        final ObservableList<String> numAppart = FXCollections.observableArrayList();
+
+        try (
+                final Connection connection = AppDataSource.INSTANCE.getConnection();
+                final Statement statement = connection.createStatement();
+                final ResultSet resultSet = statement.executeQuery("select noappart from appart");
+        ) {
+            while (resultSet.next()) {
+                numAppart.add(resultSet.getString("noappart"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        cbxImmeuble.setItems(numAppart);
+
     }
 }
+
